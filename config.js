56d699971a001475739f0a5bb26f335a420d9500
Brandon Atkins <brandon.atkins@applemarketing.net>
2019-01-05 16:05:13 -0600

    updating GA tracking ID.

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ let isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   REST_ENDPOINT: JSON.stringify('https://techcrunch.com/wp-json/wp/v2'),
   POSTS_PER_PAGE: JSON.stringify(9),
-  GA_TRACKING_ID: JSON.stringify(isProd ? '' : 'XXX'),
+  GA_TRACKING_ID: JSON.stringify(isProd ? 'UA-1860302-27' : 'XXX'),
   REQUEST_CACHE_MAX: JSON.stringify(150)
 };
